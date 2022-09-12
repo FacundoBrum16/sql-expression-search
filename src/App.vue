7c202766b57b633f33adf114f1f1b652a250eb8d
fbrum <fbrum@phoneiq.co>
2022-09-11 22:32:47 -0300

    starting project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <SqlExpressionSearch/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import SqlExpressionSearch from './pages/SqlExpressionSearch.vue'
+import './assets/tailwind.css'
+import './assets/fonts/circular/circular.css'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    SqlExpressionSearch
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Circular;
+}
+</style>
---
+++ src/pages/SqlExpressionSearch.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="bg-red-300">
+asdasd
+</div>
+</template>
+
+<script>
+export default {
+  name: 'SqlExpressionSearch',
+  props: {
+    msg: String
+  }
+}
+</script>
+
